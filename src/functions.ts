eec25741d8479549e6450beb574aeb78e578a69c
Jesse Sivonen <j.sivonen@hotmail.com>
2023-12-22 16:42:34 +0200

    Fix types

--- src/functions.ts
+++ src/functions.ts
@@ -2,9 +2,9 @@ import { NotImplementedError } from "./errors";
 import { ProviderPart, ResolverPart } from "./parts/index";
 import { Part } from "./types";
 
-export const createPart = <Type = any, Dependencies extends Part[] = []>(
+export const createPart = <Type, Dependencies extends Part[] = []>(
   definition: Part<Type> | string,
-  dependencies: [...Dependencies] = [] as unknown as Dependencies,
+  dependencies: [...Dependencies] = [] as Dependencies,
   implementation: (dependencies: {
     [Key in keyof Dependencies]: ReturnType<Dependencies[Key]>;
   }) => Type = () => {
@@ -13,7 +13,10 @@ export const createPart = <Type = any, Dependencies extends Part[] = []>(
 ): Part<Type, Dependencies> =>
   Object.assign(implementation, { definition, dependencies });
 
-export const resolvePart = (definition: Part, parts: Part[]) => {
+export const resolvePart = <Type>(
+  definition: Part<Type>,
+  parts: Part[],
+): ReturnType<typeof definition> => {
   const provider = createPart(ProviderPart, [], () => () => parts);
   const resolve = ResolverPart([() => [...parts, provider]])(ResolverPart);
   return resolve(definition);
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
-export type Part<Type = any, Dependencies extends Part[] = Part<any, any>[]> = {
-  definition: Part | string;
-  dependencies: Dependencies;
+export type Part<Type = any, Dependencies extends Part<any, any>[] = any> = {
+  definition: Part<Type, any> | string;
+  dependencies: [...Dependencies];
   (dependencies: {
     [Key in keyof Dependencies]: ReturnType<Dependencies[Key]>;
   }): Type;
